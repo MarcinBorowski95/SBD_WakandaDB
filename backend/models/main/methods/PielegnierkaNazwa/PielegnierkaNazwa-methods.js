47779cc5f654e08243e2be599c5234a435ef62b9
Warlord <bussarpl@gmail.com>
2018-05-16 19:42:01 +0200

    added abstract class

---
+++ backend/models/main/methods/PielegnierkaNazwa/PielegnierkaNazwa-methods.js
@@ -0,0 +1,7 @@
+
+model.PielegnierkaNazwa.entityMethods.Obs_pacjent = function() {
+	// Add your code here;
+};
+model.PielegnierkaNazwa.entityMethods.Tworz_Wizyta = function() {
+	// Add your code here;
+};
