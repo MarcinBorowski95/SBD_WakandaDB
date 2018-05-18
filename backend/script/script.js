afa663d859f2d461aba42eeb1cbba8a42500fe43
Marcin <bomarc2@gmail.com>
2018-05-18 20:18:20 +0200

    datafix

--- backend/script/script.js
+++ backend/script/script.js
@@ -1,4 +1,4 @@
 ds.SzpitalAbstract.remove();
-new ds.SzpitalAbstract({Id_Szpital:'1',Adres:"Kaszubska 3, Warszawa",Specjalizacja:"Ogolna"}).save();
+new ds.SzpitalAbstract({Id_Szpital:'1',Adres:"Kaszubska 3, Warszawa",Specjalizacja:"Ogolna",Abstract_ID:'1'}).save();
 ds.SzpitalAbstract.all();
 
