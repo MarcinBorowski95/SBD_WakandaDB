90e3ea73d538493e59645d0c6163291b699c1742
Warlord <bussarpl@gmail.com>
2018-05-18 16:04:46 +0200

    idk why its not working

--- backend/models/main/methods/AbstractMethod/AbstractMethod-methods.js
+++ backend/models/main/methods/AbstractMethod/AbstractMethod-methods.js
@@ -1,7 +1,3 @@
-
-model.AbstractMethod.entityMethods.Zatrud_lekarz = function() {
-	// Add your code here;
-};
-model.AbstractMethod.entityMethods.Zatrud_Pielegniarka = function() {
+model.AbstractMethod.entityMethods.Zatrud_Lekarz = function() {
 	// Add your code here;
 };
