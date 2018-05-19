6b05ef21d9e9fbd93ff85bcd0831b9ff3a4a1c88
Marcin <bomarc2@gmail.com>
2018-05-19 14:47:21 +0200

    SprAndLastFix

--- backend/models/main/methods/AbstractMethod/AbstractMethod-methods.js
+++ backend/models/main/methods/AbstractMethod/AbstractMethod-methods.js
@@ -1,3 +1,6 @@
-model.AbstractMethod.entityMethods.Zatrud_Lekarz = function() {
-	// Add your code here;
+model.AbstractMethod.entityMethods.Zatrud_Lekarz = function(imie = String , nazwisko = String, specjalizacja = String) {
+	model.LekarzNazwa.remove();
+	new model.LekarzNazwa({Imie:imie, Nazwisko: nazwisko, Specjalizacja: sspecjalizacja}).save();
+	model.LekarzNazwa.save();
+	model.LekarzNazwa.all();
 };
--- backend/script/script.js
+++ backend/script/script.js
@@ -1,4 +1,2 @@
-ds.OddzialSzpitalAbstract.remove();
-new ds.OddzialSzpitalAbstract({ID_Oddzial:'1',Adres:"Kaszubska 3, Warszawa",Specjalizacja:"Ogolna", Abstract_ID:'1', Id_Szpital:'1'}).save();
 
-ds.OddzialSzpitalAbstract.all();
+ds.LekarzNazwa.all();
